dbb5631a100a1a2a0c38dc30ec152f64d70e682a
Jeffrey Klarfeld <jeff@teeps.org>
2016-01-22 14:39:37 -0500

    Initial Commit

---
+++ Dashboard/DashboardView.h
@@ -0,0 +1,13 @@
+//
+//  DashboardView.h
+//  Dashboard
+//
+//  Created by Jeff Klarfeld on 1/22/16.
+//  Copyright © 2016 Teeps. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DashboardView : ScreenSaverView
+
+@end
---
+++ Dashboard/DashboardView.m
@@ -0,0 +1,52 @@
+//
+//  DashboardView.m
+//  Dashboard
+//
+//  Created by Jeff Klarfeld on 1/22/16.
+//  Copyright © 2016 Teeps. All rights reserved.
+//
+
+#import "DashboardView.h"
+
+@implementation DashboardView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
